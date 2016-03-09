2fc66963345228728f1059f0cb5cb4659739c015
xinji.wang <xinji.wang@transmension.com>
2016-03-09 19:43:15 +0800

    add UnionFightCrontab

---
+++ src/com/trans/pixel/service/crontab/UnionFightCrontabService.java
@@ -0,0 +1,21 @@
+package com.trans.pixel.service.crontab;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trans.pixel.service.UnionService;
+
+@Service
+public class UnionFightCrontabService {
+	@Resource
+	private UnionService unionService;
+	
+	@Scheduled(cron = "0 0 21 * * ? ")
+	@Transactional(rollbackFor=Exception.class)
+	public void unionFight() {
+		unionService.unionFightTask();
+	}
+}
