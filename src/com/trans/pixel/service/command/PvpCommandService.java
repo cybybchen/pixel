9cab07103999a4ddb5975fa328746b085913bde5
xinji.wang <xinji.wang@transmension.com>
2016-07-08 17:28:08 +0800

    rm username when helpattackmine

--- src/com/trans/pixel/service/command/PvpCommandService.java
+++ src/com/trans/pixel/service/command/PvpCommandService.java
@@ -236,7 +236,7 @@ public class PvpCommandService extends BaseCommandService {
 	}
 	
 	private void sendHelpMail(UserBean friend, UserBean user) {
-		String content = "玩家" + user.getUserName() + "帮助你赶走了矿场的敌人"; 
+		String content = "帮助你赶走了矿场的敌人"; 
 		MailBean mail = buildMail(friend.getId(), user.getId(), user.getVip(), user.getIcon(), user.getUserName(), content, MailConst.TYPE_HELP_ATTACK_PVP_MAIL);
 		mailService.addMail(mail);
 	}
