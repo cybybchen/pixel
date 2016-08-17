6009bfcb07ffde488998ce3c838d64cf81fd564d
yanbin.chen <yanbin.chen@transmension.com>
2016-08-17 18:25:33 +0800

    add buchang vip function

--- src/com/trans/pixel/model/mapper/RechargeMapper.java
+++ src/com/trans/pixel/model/mapper/RechargeMapper.java
@@ -1,9 +1,13 @@
 package com.trans.pixel.model.mapper;
 
+import java.util.List;
+
 import com.trans.pixel.model.RechargeBean;
 
 public interface RechargeMapper {
 	
 	public int insertUserRechargeRecord(RechargeBean recharge);
+	
+	public List<RechargeBean> getRechargeRecord();
 
 }
