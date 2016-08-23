ea3f60ff0140699dc0404e8914fd5d8d9be6487e
xinji.wang <xinji.wang@transmension.com>
2016-08-23 18:52:43 +0800

    add areafightTask

---
+++ src/com/trans/pixel/service/crontab/AreaFightCrontabService.java
@@ -0,0 +1,20 @@
+package com.trans.pixel.service.crontab;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.trans.pixel.service.AreaFightService;
+
+@Service
+public class AreaFightCrontabService {
+	@Resource
+	private AreaFightService areaFightService;
+	
+	@Scheduled(cron = "0 0/1 * * * ? ")
+//	@Transactional(rollbackFor=Exception.class)
+	public void areaFight() {
+		areaFightService.calFight();
+	}
+}
