715a7633365bc3df942ee8d33e68c496ce5b2836
yanbin.chen <yanbin.chen@transmension.com>
2016-08-30 16:19:44 +0800

    del call brother vip level limit

--- src/com/trans/pixel/service/command/MailCommandService.java
+++ src/com/trans/pixel/service/command/MailCommandService.java
@@ -30,7 +30,7 @@ import com.trans.pixel.service.redis.RedisService;
 
 @Service
 public class MailCommandService extends BaseCommandService {
-	private static final int LIMIT_VIP_LEVEL = 1;
+	private static final int LIMIT_VIP_LEVEL = 0;
 	@Resource
 	private MailService mailService;
 	@Resource
