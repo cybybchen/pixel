12dff05db0812b59240be445c579468a430cbfd2
yanbin.chen <yanbin.chen@transmension.com>
2016-10-24 11:22:13 +0800

    fix bug of union crontab boss refresh

--- src/com/trans/pixel/service/UnionService.java
+++ src/com/trans/pixel/service/UnionService.java
@@ -942,6 +942,12 @@ public class UnionService extends FightService{
 		
 		if (unionBossRecord != null) {
 			Date bossStartTime = DateUtil.getFutureHour(DateUtil.getDate(unionBossRecord.getStartTime()), boss.getRefreshtime());
+			String bossEnd = getUnionBossEndTime(union, boss.getId());
+			if (!bossEnd.isEmpty()) {
+				if (DateUtil.getDate(bossEnd).after(bossStartTime)) {
+					return false;
+				}
+			}
 			Date bossEndTime = DateUtil.getFutureDay(DateUtil.getDate(unionBossRecord.getEndTime()), 1);
 			if (current.before(bossEndTime) && current.after(bossStartTime))
 				return true;
