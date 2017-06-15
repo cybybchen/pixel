5814beb2674b32034e721f52ac86685bfc40b859
yanbin.chen <yanbin.chen@transmension.com>
2017-06-15 19:01:25 +0800

    add cost const config

---
+++ src/com/trans/pixel/constants/CostConst.java
@@ -0,0 +1,8 @@
+package com.trans.pixel.constants;
+
+public class CostConst {
+	public static final int JEWEL_TO_COIN = 1000;
+	public static final int JEWEL_TO_RONGYU = 10;
+	public static final int JEWEL_TO_MOJING = 20;
+	public static final int ZHAOHUANSHI_TO_JEWEL = 200;
+}
