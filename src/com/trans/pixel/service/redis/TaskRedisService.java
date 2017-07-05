bb18e51c6a4d80deb883a7f2208232707b6b84af
yanbin.chen <yanbin.chen@transmension.com>
2017-07-05 12:20:21 +0800

    fix bug of task2 parse

--- src/com/trans/pixel/service/redis/TaskRedisService.java
+++ src/com/trans/pixel/service/redis/TaskRedisService.java
@@ -193,7 +193,7 @@ public class TaskRedisService extends RedisService {
 		for (Task2TargetHero.Builder hero : builder.getDataBuilderList()) {
 //			Map<String, String> redismap = new HashMap<String, String>();
 //			for (TaskTarget.Builder task : hero.getTargetBuilderList()) {
-				map.put("" + hero.getHeroid() + hero.getTargetid(), hero.build());
+				map.put("" + hero.getHeroid(), hero.build());
 //				for (TaskOrder.Builder order : task.getOrderBuilderList()) {
 //					order.setTargetid(task.getTargetid());
 //					redismap.put("" + order.getOrder(), formatJson(order.build()));
