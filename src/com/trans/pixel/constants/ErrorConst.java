c2576eca40458ad4062784559f2c9d256c9a5ce0
yanbin.chen <yanbin.chen@transmension.com>
2017-07-05 18:31:43 +0800

    modify laddermode error tip

--- src/com/trans/pixel/constants/ErrorConst.java
+++ src/com/trans/pixel/constants/ErrorConst.java
@@ -56,7 +56,7 @@ public enum ErrorConst implements ResultConst {
     CAN_NOT_GIVEUP_REWARDTASK_ERROR(4026, "无法放弃该悬赏"), REWARDTASK_IS_LIMIT_ERROR(4027, "悬赏任务已达上限"), EQUIP_LEVEL_IS_LIMIT_ERROR(4028, "装备等级已达上限"),
     EQUIP_IS_EXIST_ERROR(4029, "装备已存在，无法合成第二件"), TIME_IS_NOT_OVER_ERROR(4030, "等待时间未到，请耐心一会"),
     
-    LADDER_SEASON_IS_END_ERROR(5001, "该赛季已结束，请退出天梯重新进入！"), LADDER_SEASON_IS_NOT_OPEN_ERROR(5002, "该赛季还未开启，请等待开启！"),
+    LADDER_SEASON_IS_END_ERROR(5001, "该赛季已结束，请退出竞技场重新进入！"), LADDER_SEASON_IS_NOT_OPEN_ERROR(5002, "该赛季还未开启，请等待开启！"),
     SAVINGBOX_BUY_TYPE_ERROR(5003, "淘金罐购买类型异常"), EQUIP_NOT_GET_ERROR(5004, "未获得该装备！"), SAVINGBOX_LEVEL_IS_LIMIT_ERROR(5005, "淘金罐等级已达上限"),
     LADDER_SEASON_EQUIP_NOTUP_ERROR(5006, "当前赛季内无法升级该装备！"), CHIP_IS_NOT_EXISTS_ERROR(5007, "该碎片不存在！"),
     CHIP_CAN_NOT_GET_ERROR(5007, "无法兑换该碎片！"), EQUIP_IS_EXISTS_ERROR(5008, "装备已存在，无法合成！"),
