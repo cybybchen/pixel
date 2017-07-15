60b58ec9e7096e1a038b778ee213315b43002169
yanbin.chen <yanbin.chen@transmension.com>
2017-07-15 14:42:59 +0800

    Revert "not insert into ladder room when fight fail"
    This reverts commit 57bda67048ee09358ce9a814b04d3a2bf162d8ce.

--- src/com/trans/pixel/service/LadderService.java
+++ src/com/trans/pixel/service/LadderService.java
@@ -140,8 +140,7 @@ public class LadderService {
 			builder.setTaskProcess(builder.getTaskProcess() + 1);
 		userLadderService.updateUserLadder(builder.build());
 		
-		if (ret == 0)
-			userLadderService.storeRoomData(builder.build(), type, userLadder.getGrade());
+		userLadderService.storeRoomData(builder.build(), type, userLadder.getGrade());
 		
 		if (builder.getScore() > userLadder.getScore()) {//插入排行榜
 			userLadderService.addLadderRank(user, builder.build());
