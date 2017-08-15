6ddf73e7e15023a100cfc063e2b10da7f82594f2
yanbin.chen <yanbin.chen@transmension.com>
2017-08-15 15:09:31 +0800

    fix get fightinfo ranksize from 50 to 10

--- src/com/trans/pixel/constants/RankConst.java
+++ src/com/trans/pixel/constants/RankConst.java
@@ -27,6 +27,7 @@ public class RankConst {
 	public static final int RAID_RANK_PREFIX = 100;
 	
 	public static final int FIGHTINFO_RANK_LIMIT = 50;
+	public static final int FIGHTINFO_RANK_RETURN_LIMIT = 10;
 	
 	public static final int TITLE_RANK_START = 0;
 	public static final int TITLE_RANK_END = 19;
--- src/com/trans/pixel/service/command/RankCommandService.java
+++ src/com/trans/pixel/service/command/RankCommandService.java
@@ -1,9 +1,11 @@
 package com.trans.pixel.service.command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.stereotype.Service;
 
 import com.trans.pixel.constants.RankConst;
@@ -11,6 +13,7 @@ import com.trans.pixel.model.userinfo.UserBean;
 import com.trans.pixel.model.userinfo.UserRankBean;
 import com.trans.pixel.protoc.ActivityProto.RequestRankCommand;
 import com.trans.pixel.protoc.ActivityProto.ResponseRankCommand;
+import com.trans.pixel.protoc.Base.FightInfo;
 import com.trans.pixel.protoc.Base.UserRank;
 import com.trans.pixel.protoc.Commands.ResponseCommand.Builder;
 import com.trans.pixel.service.RankService;
@@ -25,7 +28,16 @@ public class RankCommandService extends BaseCommandService {
 		ResponseRankCommand.Builder builder = ResponseRankCommand.newBuilder();
 		int type = cmd.getType();
 		if (type == RankConst.TYPE_FIGHTINFO) {
-			builder.addAllFightInfo(rankService.getFightInfoList());
+			List<FightInfo> fightinfoList = rankService.getFightInfoList();
+			List<FightInfo> randomFightinfo = new ArrayList<FightInfo>();
+			while (fightinfoList.size() >= RankConst.FIGHTINFO_RANK_RETURN_LIMIT &&
+					randomFightinfo.size() < RankConst.FIGHTINFO_RANK_RETURN_LIMIT) {
+				int rand = RandomUtils.nextInt(fightinfoList.size());
+				FightInfo fight = fightinfoList.get(rand);
+				randomFightinfo.add(fight);
+				fightinfoList.remove(rand);
+			}
+			builder.addAllFightInfo(randomFightinfo);
 		} else {
 			List<UserRankBean> rankList = rankService.getRankList(user.getServerId(), type);
 			List<UserRank> userRankBuilderList = buildUserRankList(rankList);
