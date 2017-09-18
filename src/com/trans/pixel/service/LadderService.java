d4ec5737559e8c05e8a96510e8b0a6bed6a50c3c
yanbin.chen <yanbin.chen@transmension.com>
2017-09-18 18:22:17 +0800

    fix bug of laddermoderewardcount add

--- src/com/trans/pixel/service/LadderService.java
+++ src/com/trans/pixel/service/LadderService.java
@@ -155,7 +155,7 @@ public class LadderService {
 		//最佳阵容奖励
 		if (ret == 0 && enemy.getPosition() == 0 && enemy.getTeam().getUser().getId() < 0 && user.getLadderModeRewardCount() < 5) {
 			rewards.addAllLoot(userLadderService.getLadderTeamReward());
-			user.setLadderModeRewardCount(user.getLadderModeRewardCount() - 1);
+			user.setLadderModeRewardCount(user.getLadderModeRewardCount() + 1);
 		}
 		
 		Map<String, String> params = new HashMap<String, String>();
